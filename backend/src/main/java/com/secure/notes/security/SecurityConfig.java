68a61609a59bbdb408cbc41a0ee9ac53516cf84c
Douglas Silva <douglassnascimet@gmail.com>
2024-10-08 09:33:36 -0300

    Add files via upload

---
+++ backend/src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.secure.notes.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // Quando eu declaro um novo filtro de segurança, com o @Bean, ele sobrescreve o filtro padrão do Spring Security
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        // Faz com que todas as requisições necessitam de autenticação
+        http.authorizeHttpRequests((request) -> request.anyRequest().authenticated());
+        //http.formLogin(Customizer.withDefaults());
+        // Diz que a autenticação utilizada é a básica do Spring Security
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
