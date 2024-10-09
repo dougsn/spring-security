50d5c5d86233aee638667600f71303015604ad90
dnascimento <douglassnascimet@gmail.com>
2024-10-09 13:56:58 -0300

    add: Criando AuthenticationFilter para criar usuários na memória da aplicação.

--- backend/src/main/java/com/secure/notes/security/SecurityConfig.java
+++ backend/src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -6,7 +6,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -32,6 +34,33 @@ public class SecurityConfig {
         // Diz que a autenticação utilizada é a básica do Spring Security
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    // Criando diversos usuários na memória do backend, para poderem ser utilizados na aplicação.
+
+    // O {noop} diz para o Spring Security que a senha não será criptografada, será "salva" como texto simples.
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager =
+                new InMemoryUserDetailsManager();
+        if (!manager.userExists("user1")) {
+            manager.createUser(
+                    User.withUsername("user1")
+                            .password("{noop}password1")
+                            .roles("USER")
+                            .build()
+            );
+        }
+        if (!manager.userExists("admin")) {
+            manager.createUser(
+                    User.withUsername("admin")
+                            .password("{noop}adminPass")
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+        return manager;
     }
 
 }
