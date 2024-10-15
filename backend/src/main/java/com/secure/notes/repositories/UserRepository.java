cd56193cab209d3edc098c9ddfc9ce1edf449956
dnascimento <douglassnascimet@gmail.com>
2024-10-15 09:42:06 -0300

    add: Criando métodos para criação de conta e login na aplicação, também foi criado métodos para buscar os dados do usuário com base no token e no nome do usuário.

--- backend/src/main/java/com/secure/notes/repositories/UserRepository.java
+++ backend/src/main/java/com/secure/notes/repositories/UserRepository.java
@@ -10,5 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserName(String username);
 
-    boolean existsByUserName(String username);
+    Boolean existsByEmail(String email);
+
+    Boolean existsByUserName(String username);
 }
