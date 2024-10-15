90a91a6904c72e436933126ec970b936a4c1b482
dnascimento <douglassnascimet@gmail.com>
2024-10-15 16:31:28 -0300

    add: Criando tabela de auditoria para todas as manipulações da entidade Note.

--- backend/src/main/java/com/secure/notes/services/imp/NoteService.java
+++ backend/src/main/java/com/secure/notes/services/imp/NoteService.java
@@ -2,6 +2,7 @@ package com.secure.notes.services.imp;
 
 import com.secure.notes.models.Note;
 import com.secure.notes.repositories.NoteRepository;
+import com.secure.notes.services.AuditLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +14,17 @@ public class NoteService implements com.secure.notes.services.NoteService {
     @Autowired
     private NoteRepository repository;
 
+    @Autowired
+    private AuditLogService auditLogService;
+
     @Override
     public Note createNoteForUser(String username, String content) {
         Note note = new Note();
         note.setContent(content);
         note.setOwnerUsername(username);
-        return repository.save(note);
+        repository.save(note);
+        auditLogService.logNoteCreation(username, note);
+        return note;
     }
 
     @Override
@@ -26,12 +32,14 @@ public class NoteService implements com.secure.notes.services.NoteService {
         Note note = repository.findById(noteId)
                 .orElseThrow(() -> new RuntimeException("Note not found."));
         note.setContent(content);
+        auditLogService.logNoteUpdate(username, note);
         return repository.save(note);
     }
 
     @Override
     public void deleteNoteForUser(Long noteId, String username) {
         if (repository.findById(noteId).isPresent()) {
+            auditLogService.logNoteDeletion(username, noteId);
             repository.deleteById(noteId);
         } else {
             throw new RuntimeException("This note not found.");
